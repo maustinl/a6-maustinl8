96c2fcb66b5d379916b45c6ab75c70542dcc2468
matthewlewis <Austin_Lewis@kenan-flagler.unc.edu>
2019-11-05 15:48:31 -0500

    commit2

--- src/a6/ProximityIterator.java
+++ src/a6/ProximityIterator.java
@@ -1,12 +1,13 @@
 package a6;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-// Collaborated w Marcus H.
+// Collaborated w Marcus H. and 401 LAs
 
 public class ProximityIterator implements Iterator<Driver> {
 
-    private Iterator<Driver> driver_pool;
+    private Iterator<Driver> iterator;
     private Position client_position;
     private int proximity_limit;
     
@@ -14,7 +15,7 @@ public class ProximityIterator implements Iterator<Driver> {
 
 
     public ProximityIterator(Iterable<Driver> driver_pool, Position client_position, int proximity_limit) {
-        this.driver_pool = driver_pool.iterator();
+        this.iterator = driver_pool.iterator();
         this.client_position = client_position;
         this.proximity_limit = proximity_limit;
         this.nextDriver = null;
@@ -36,22 +37,34 @@ public class ProximityIterator implements Iterator<Driver> {
     @Override
     public boolean hasNext() {
 
-        while (driver_pool.hasNext()) {
+        if (!(nextDriver == null)) {
+            return true;
+        }
 
-            nextDriver = driver_pool.next();
+        while (iterator.hasNext()) {
+
+            nextDriver = iterator.next();
 
             if (proximity_limit >= client_position.getManhattanDistanceTo(nextDriver.getVehicle().getPosition())) {
                 return true;
             }
 
         }
+        nextDriver = null;
         return false;
 
     }
 
     @Override
     public Driver next() {
-        return null;
+
+        if(!(hasNext())) {
+            throw new NoSuchElementException("No more drivers were found");
+        }
+
+        Driver driver = nextDriver;
+        nextDriver = null;
+        return driver;
     }
 
 
